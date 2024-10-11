01b5d17979d4cc645ab0349e4ab53ab6f9c864ad
Andrei Mukamolau <amv@bamboogroup.eu>
2024-10-11 21:29:55 +0300

    Load map via JS

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -1,5 +1,8 @@
+from typing import Any, Sequence
+from django import forms
 from django.conf import settings
 from django.forms import widgets
+from django.utils.html import format_html
 
 
 class GoogleMapsAddressWidget(widgets.TextInput):
@@ -11,7 +14,23 @@ class GoogleMapsAddressWidget(widgets.TextInput):
             'all': ('django_google_maps/css/google-maps-admin.css', )
         }
         js = (
-            'https://maps.google.com/maps/api/js?key={}&loading=async&callback=initMap&libraries=maps,marker,places'.format(
-                settings.GOOGLE_MAPS_API_KEY),
             'django_google_maps/js/google-maps-admin.js',
         )
+
+    def get_context(self, name, value, attrs) -> dict[str, Any]:
+        context = super().get_context(name, value, attrs)
+
+        context["api_key"] = settings.GOOGLE_MAPS_API_KEY
+        context["map_id"] = settings.GOOGLE_MAPS_MAP_ID
+
+        return context
+
+def render_js(cls):
+    return [
+        format_html(
+            '<script src="{}" defer></script>',
+            cls.absolute_path(path)
+        ) for path in cls._js
+    ]
+
+forms.widgets.Media.render_js = render_js
