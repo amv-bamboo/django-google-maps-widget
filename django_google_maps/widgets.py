917918c473da019a20607d22a8e1fc7cc8e1e29e
Andrei Mukamolau <amv@bamboogroup.eu>
2024-10-31 16:15:05 +0200

    Fixed flake errors

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -1,4 +1,4 @@
-from typing import Any, Sequence
+from typing import Any
 from django import forms
 from django.conf import settings
 from django.forms import widgets
@@ -7,15 +7,14 @@ from django.utils.html import format_html
 
 class GoogleMapsAddressWidget(widgets.TextInput):
     """a widget that will place a google map right after the #id_address field"""
+
     template_name = "django_google_maps/widgets/map_widget.html"
 
     class Media:
-        css = {
-            'all': ('django_google_maps/css/google-maps-admin.css', )
-        }
+        css = {"all": ("django_google_maps/css/google-maps-admin.css",)}
         js = (
-            f'https://maps.googleapis.com/maps/api/js?key={settings.GOOGLE_MAPS_API_KEY}&libraries=maps,marker,places,geocoding',
-            'django_google_maps/js/google-maps-admin.js',
+            f"https://maps.googleapis.com/maps/api/js?key={settings.GOOGLE_MAPS_API_KEY}&libraries=maps,marker,places,geocoding",
+            "django_google_maps/js/google-maps-admin.js",
         )
 
     def get_context(self, name, value, attrs) -> dict[str, Any]:
@@ -26,12 +25,12 @@ class GoogleMapsAddressWidget(widgets.TextInput):
 
         return context
 
+
 def render_js(cls):
     return [
-        format_html(
-            '<script src="{}" defer></script>',
-            cls.absolute_path(path)
-        ) for path in cls._js
+        format_html('<script src="{}" defer></script>', cls.absolute_path(path))
+        for path in cls._js
     ]
+
 
 forms.widgets.Media.render_js = render_js
